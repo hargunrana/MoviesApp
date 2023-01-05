e342d2139efe6a01dffbd3f2b789ea3ebe81e14b
hargunrana <hargun.rana1@gmail.com>
2023-01-05 15:58:58 +0530

    project started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Banner from "./Components/Banner";
+function App() {
+    return (
+        <>
+            <NavBar />
+            <Banner />
+        </>
+    );
+}
+
+export default App;
